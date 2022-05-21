fe51e321243fbb837fabcb5e1bd7fbdf9992dbd3
AngelMcDaniel <105662216+AngelMcDaniel@users.noreply.github.com>
2022-05-21 10:23:31 -0500

    Add files via upload

---
+++ Activity2.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Activity_2
+{
+    partial class Activity2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.inputBox1 = new System.Windows.Forms.TextBox();
+            this.outputBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(132, 182);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(86, 34);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Submit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // inputBox1
+            // 
+            this.inputBox1.Location = new System.Drawing.Point(123, 71);
+            this.inputBox1.Name = "inputBox1";
+            this.inputBox1.Size = new System.Drawing.Size(110, 20);
+            this.inputBox1.TabIndex = 1;
+            this.inputBox1.TextChanged += new System.EventHandler(this.inputBox1_TextChanged);
+            // 
+            // outputBox2
+            // 
+            this.outputBox2.Location = new System.Drawing.Point(123, 138);
+            this.outputBox2.Name = "outputBox2";
+            this.outputBox2.Size = new System.Drawing.Size(108, 20);
+            this.outputBox2.TabIndex = 2;
+            this.outputBox2.TextChanged += new System.EventHandler(this.outputBox2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Enter Tablespoons";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(49, 138);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Number of tsp";
+            // 
+            // Activity2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(311, 273);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.outputBox2);
+            this.Controls.Add(this.inputBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Activity2";
+            this.Text = "Cooking Converter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox inputBox1;
+        private System.Windows.Forms.TextBox outputBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Activity2.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Activity_2
+{
+    //Angel McDaniel
+    public partial class Activity2 : Form
+    {
+        string numConverter;
+
+        public Activity2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            outputBox2.Text = numConverter;
+
+            string message = outputBox2.Text;
+            string title = "Message Box";
+            MessageBox.Show(message, title);
+        }
+
+        private void inputBox1_TextChanged(object sender, EventArgs e)
+        {
+            double tempDouble;
+
+            try
+            {
+                tempDouble = Convert.ToDouble(inputBox1.Text);
+                tempDouble *= 3;
+                tempDouble = Math.Round(tempDouble, 4);
+                numConverter = tempDouble.ToString("0.000");
+                //numConverter = Convert.ToString(tempDouble);
+            }
+            catch
+            {
+                numConverter = "Error";
+            }
+        }
+
+        private void outputBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
